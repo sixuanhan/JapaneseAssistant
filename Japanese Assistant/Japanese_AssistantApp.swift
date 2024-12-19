e38f8573ada0f20338b2d198daf86715c976927d
Sixuan Han <sixuan.han.25@dartmouth.edu>
2024-12-19 16:03:11 +0800

    Initial Commit

---
+++ Japanese Assistant/Japanese_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Japanese_AssistantApp.swift
+//  Japanese Assistant
+//
+//  Created by xuanxuan on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Japanese_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
